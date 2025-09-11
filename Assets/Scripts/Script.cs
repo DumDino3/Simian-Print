d7cecead64520574bc616dd3526d8b9bdf3acab2
DumDino <tqhieu10072004@gmail.com>
2025-09-11 10:13:04 +0700

    URPify

---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,24 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Script : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) )
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        this.transform.position = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+    }
+}
